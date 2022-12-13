54156ba8456c9d3412b62486afca2c2f0f5b51c2
Raphael Cardoso <cardoso.raphael@hotmail.com>
2022-12-13 09:16:35 -0300

    Adicionar arquivos de projeto.

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
